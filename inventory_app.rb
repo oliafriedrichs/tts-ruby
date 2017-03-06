ed0bd95f8fa83f04e68f4a4255c463d97d63a762
Olia Friedrichs <oliafriedrichs@icloud.com>
2017-03-06 17:56:35 -0600

    homework update

--- inventory_app.rb
+++ inventory_app.rb
@@ -1,7 +1,17 @@
 # Is built around a class and methods for that class.
+class Inventory
+
+  def initialize
+    puts "Welcome"
+    @inventory = {tshirts: 27, dresses: 52, shoes: 75, pants: 7}
+   end
+
 
 # Lists several items.
 
+
+
+end
 # Allows a user to select an item and view the inventory count, change the number of the inventory, delete the item altogether, or even change the name.
 
 # Create a new item and give the item an inventory count, and have this item now show up in the list of items you can select.
