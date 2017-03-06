0286c81d3b7f634783f4c604421b7b35c69c77df
Olia Friedrichs <oliafriedrichs@icloud.com>
2017-03-06 17:42:52 -0600

    Homework

---
+++ inventory_app.rb
@@ -0,0 +1,9 @@
+# Is built around a class and methods for that class.
+
+# Lists several items.
+
+# Allows a user to select an item and view the inventory count, change the number of the inventory, delete the item altogether, or even change the name.
+
+# Create a new item and give the item an inventory count, and have this item now show up in the list of items you can select.
+
+# Loops through until the user does not want to edit the list anymore.
